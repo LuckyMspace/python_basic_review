c760d63b7112fa615aadc12dc60a342f686578d6
Myeong <caps813@gmail.com>
2023-02-11 21:55:24 +0900

    2023-02-11(1)

--- Error_Dictionary.py
+++ Error_Dictionary.py
@@ -1,18 +1,21 @@
-'''TypeError: 'int' object is not subscriptable''' # 오류 발생
+""" TypeError: 'int' object is not subscriptable """ 
 #정수형(int)에서 '인덱싱' 및 '슬라이싱'을 시도할 때 발생하는 오류
 #자료형 관련 에러다. 
 
-"""TypeError: string indices must be integers, not 'tuple'"""
+""" TypeError: string indices must be integers, not 'tuple' """
 # 문자열 인덱스는 'tuple'이 아닌 정수여야 합니다.
 # indices는 index의 복수형 영단어.
 
-"""'str' object is not callable"""
+""" 'str' object is not callable """
 #'str' 개체를 호출할 수 없습니다.
 #
 
-"""TypeError: replace expected at most 3 arguments, got 4"""
+""" TypeError: replace expected at most 3 arguments, got 4 """
 # Replace 메서드에서 발생한 에러. '최대 3개의 인수를 교체해야 합니다. 4개를 받았습니다'
 
-"""endswith() takes at least 1 argument (0 given)"""
+""" endswith() takes at least 1 argument (0 given) """
 # endswith() 메서드를 공백으로 입력했을때 발생했다.
 # 어떤 문자열이 확인하는 것이기 때문에 문자열을 입력해 줘야한다.
+
+""" leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers """
+# 10진수에서 정수가 0으로 시작할 때 발생
